88631bc23a1970270a6f8fa2e4f49152746441ae
woojeans1204 <woojeans1204@gmail.com>
2024-09-30 22:30:28 +0900

    라이브 코딩 클론

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
 import './reset.css';
 
-import { useReducer } from 'react';
+import { useEffect, useState } from 'react';
+
+import styles from './App.module.css';
+
+type TodoResponse = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export const App = () => {
-  const [count, increment] = useReducer((c: number) => c + 1, 0);
+  const [id, setId] = useState(1);
+  const [data, setData] = useState<TodoResponse>();
 
-  return <button onClick={increment}>{count}</button>;
+  useEffect(() => {
+    let ignore = false;
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((response) => response.json() as Promise<TodoResponse>)
+      .then((response) => {
+        if (ignore) return;
+        setData(response);
+      })
+      .catch(() => {
+        window.alert();
+      });
+
+    return () => {
+      ignore = true;
+    };
+  }, [id]);
+
+  return (
+    <div className={styles.wrapper}>
+      <button
+        onClick={() => {
+          setId(id - 1);
+        }}
+      >
+        왼쪽
+      </button>
+      <div>
+        가운데 영역{' '}
+        <div>
+          {id} - {data?.id} - {data?.title}
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          setId(id + 1);
+        }}
+      >
+        오른쪽
+      </button>
+    </div>
+  );
 };
